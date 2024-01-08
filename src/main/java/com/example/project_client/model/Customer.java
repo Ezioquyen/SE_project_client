40efd85b1e640b09df9e6c8fa22c0b3d346091b3
yinnxing <yinnxing@gmail.com>
2024-01-08 13:19:12 +0700

    ggg

--- src/main/java/com/example/project_client/model/Customer.java
+++ src/main/java/com/example/project_client/model/Customer.java
@@ -1,11 +1,13 @@
 package com.example.project_client.model;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Data
 @NoArgsConstructor
+@Getter
 public class Customer {
     private Integer id = 0;
     private String phoneNumber = "";
